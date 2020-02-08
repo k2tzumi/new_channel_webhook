8676fb16e85b8dda67e56099ccbfcbaa4bb58c9f
k2tzumi <katzumi+github@gmail.com>
2020-02-08 18:11:26 +0900

    introduced tslint

--- src/Code.ts
+++ src/Code.ts
@@ -1,90 +1,98 @@
 const properties = PropertiesService.getScriptProperties();
-const VERIFICATION_TOKEN: string = properties.getProperty('VERIFICATION_TOKEN')
+const VERIFICATION_TOKEN: string = properties.getProperty("VERIFICATION_TOKEN");
 
 function doPost(e): GoogleAppsScript.Content.TextOutput {
-  let postData = JSON.parse(e.postData.getDataAsString())
+  const postData = JSON.parse(e.postData.getDataAsString());
   if (postData.token !== VERIFICATION_TOKEN) {
-    console.warn('Invalid verification token: %s', postData.token)
-    throw new Error('Invalid verification token.')
+    console.warn("Invalid verification token: %s", postData.token);
+    throw new Error("Invalid verification token.");
   }
 
-  let res = {}
+  let res = {};
   switch (postData.type) {
-    case 'url_verification':
-      console.log({ message: 'url_verification called.', data: postData.challenge })
-      res = { 'challenge': postData.challenge }
-      break
-    case 'event_callback':
-      console.log({ message: 'event_callback called.', data: postData })
+    case "url_verification":
+      console.log({
+        message: "url_verification called.",
+        data: postData.challenge
+      });
+      res = { challenge: postData.challenge };
+      break;
+    case "event_callback":
+      console.log({ message: "event_callback called.", data: postData });
       if (!isEventIdProceeded(postData.event_id)) {
-        res = eventHandler(postData.event)
+        res = eventHandler(postData.event);
       } else {
-        console.warn({ message: 'event_callback duplicate called.', data: postData.event_id })
-        res = { 'duplicated': postData.event_id }
+        console.warn({
+          message: "event_callback duplicate called.",
+          data: postData.event_id
+        });
+        res = { duplicated: postData.event_id };
       }
-      break
+      break;
     default:
-      console.error({ message: 'unknown event called.', data: postData.type })
-      res = { 'unknown event': postData.type }
-      break
+      console.error({ message: "unknown event called.", data: postData.type });
+      res = { "unknown event": postData.type };
+      break;
   }
 
-  return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(ContentService.MimeType.JSON);
+  return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(
+    ContentService.MimeType.JSON
+  );
 }
 
 function eventHandler(event: string[]) {
-  if (event['type'] === 'channel_created') {
-    return channelCreated(event)
+  if (event.type === "channel_created") {
+    return channelCreated(event);
   }
 
-  console.error(`unsupported data ${event}`)
-  return { 'unsupported': event }
+  console.error(`unsupported data ${event}`);
+  return { unsupported: event };
 }
 
-const NOTIFICATION_MESSAGE: string = properties.getProperty('NOTIFICATION_MESSAGE') || 'A public channel created :point_right: '
+const NOTIFICATION_MESSAGE: string =
+  properties.getProperty("NOTIFICATION_MESSAGE") ||
+  "A public channel created :point_right: ";
 
 function channelCreated(event: string[]) {
-  let channel = event["channel"]
-  let message = `${NOTIFICATION_MESSAGE} ${channelLink(channel)}`
+  const channel = event.channel;
+  const message = `${NOTIFICATION_MESSAGE} ${channelLink(channel)}`;
 
-  postSlack(message)
+  postSlack(message);
 
-  return { 'posted': message }
+  return { posted: message };
 }
 
 function channelLink(channel: string[]): string {
-  return `<##${channel["id"]}|#${channel["name"]}>`
+  return `<##${channel.id}|#${channel.name}>`;
 }
 
 function isEventIdProceeded(eventId: string): boolean {
-  let cash = CacheService.getScriptCache();
-  let prevEventId = cash.get(eventId);
+  const cash = CacheService.getScriptCache();
+  const prevEventId = cash.get(eventId);
   if (prevEventId) {
-    return true
+    return true;
   } else {
-    cash.put(eventId, 'proceeded', 60);
-    return false
+    cash.put(eventId, "proceeded", 60);
+    return false;
   }
 }
 
-const POST_URL: string = properties.getProperty('INCOMING_WEBHOOKS_URL')
-const USER_NAME: string = 'new_channel_bot'
-const ICON: string = ':new:'
+const POST_URL: string = properties.getProperty("INCOMING_WEBHOOKS_URL");
+const USER_NAME: string = "new_channel_bot";
+const ICON: string = ":new:";
 
 function postSlack(message: string): void {
-  let jsonData =
-  {
-    "username": USER_NAME,
-    "icon_emoji": ICON,
-    "text": message
-  }
+  const jsonData = {
+    username: USER_NAME,
+    icon_emoji: ICON,
+    text: message
+  };
 
-  let options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions =
-  {
-    "method": "post",
-    "contentType": "application/json",
-    "payload": JSON.stringify(jsonData)
-  }
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify(jsonData)
+  };
 
-  UrlFetchApp.fetch(POST_URL, options)
+  UrlFetchApp.fetch(POST_URL, options);
 }
